01459bedf12e1db6ac95b4e278eaa94942b2ce8a
Rachel <rad044@latech.edu>
2026-03-19 10:30:30 -0500

    initial commit

---
+++ 1/template.py
@@ -0,0 +1,22 @@
+# Et tu, Brute?
+#
+# Team Name: Hashbrowns
+# Members: 
+# A program that attempts to solve cipher texts that are shifted by some number.
+
+import sys
+
+# the alphabet
+ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`~!@#$%^&*()-_=+[{]}\\|;:'\",<.>/? "
+
+# read ciphertext
+ciphertext = sys.stdin.read()
+
+# find letter frequencies
+
+# generate candidate plaintexts
+
+# screen plaintexts with dictionary
+
+# output plaintext and shift
+
