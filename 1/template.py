d00b58939aba458d053b2130bcae142351bd8240
Rachel <rad044@latech.edu>
2026-03-19 10:54:51 -0500

    plaintext generation added

--- 1/template.py
+++ 1/template.py
@@ -12,9 +12,36 @@ ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`~!@#$
 # read ciphertext
 ciphertext = sys.stdin.read()
 
-# find letter frequencies
-
 # generate candidate plaintexts
+poss_plaintexts = []
+
+for shift in range(len(ALPHABET)):
+
+    current_plaintext = []
+
+    for char in ciphertext:
+        print(char)
+
+        #find char in alphabet
+        try:
+            position = ALPHABET.index(char)
+
+            #shift
+            if position + shift >= len(ALPHABET):
+                position = (position + shift) - len(ALPHABET)
+            else:
+                position += shift
+
+            # add char to plaintext
+            current_plaintext.append(ALPHABET[position])
+        except ValueError: # append chars not in alphabet directly
+            current_plaintext.append(char)
+    
+    # add plaintext to list of possibilities
+    poss_plaintexts.append("".join(current_plaintext))
+
+for text in poss_plaintexts:
+    print(f"{text}\n")
 
 # screen plaintexts with dictionary
 
